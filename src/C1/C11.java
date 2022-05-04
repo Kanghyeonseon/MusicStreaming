e2e45ea01b7bc47846947133ecfb4f1ac5c525b3
Kanghyeonseon <102352988+Kanghyeonseon@users.noreply.github.com>
2022-05-04 11:23:49 +0900

    C1123

--- src/C1/C11.java
+++ src/C1/C11.java
@@ -2,11 +2,9 @@ package C1;
 
 public class C11 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("helloeeeeeeeeee");
 		System.out.println("eeefefwjie ofjweoifjweoiufjh");
-=======
+
 		System.out.println("hello World");
->>>>>>> branch 'master' of https://github.com/Kanghyeonseon/MusicStreaming.git
 	}
 }
