e780506911c8221554d58eaf606c4856c2146a59
Kanghyeonseon <102352988+Kanghyeonseon@users.noreply.github.com>
2022-05-09 17:56:37 +0900

    Hyeonseon (#17)
    * hi?
    
    * view

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,13 @@ package main;
 
 import view.GUIViewer;
 import view.Viewer;
-//ㅑㅕㅓㅏㅕㅘㅕㅏㅛㅕㅏ
 public class Main {
 	public static void main(String[] args) {
 
 
 //		Viewer view1 = new Viewer();
 //		view1.LoginMenu();
+
 
 		new GUIViewer();
 	}
